7c88dbaccb4364f52785b4d51feb98b2bcc0322a
wangzhifeng <wangzhifeng@aiops.com>
2019-11-20 17:40:42 +0800

    add diesel learning

--- dotenv/src/main.rs
+++ dotenv/src/main.rs
@@ -6,22 +6,18 @@ extern crate dotenv_codegen;
 use std::env;
 
 fn main() {
-
-
     // use_method();
 
     use_macro();
-
-
 }
 
 fn use_method() {
     dotenv::from_path(".env").ok();
 
     for (key, value) in env::vars() {
-//        if key == "name".to_string() {
-//            println!("{} : {}", key, value);
-//        }
+        //        if key == "name".to_string() {
+        //            println!("{} : {}", key, value);
+        //        }
 
         println!("{}: {}", key, value);
     }
@@ -29,5 +25,5 @@ fn use_method() {
 
 // dotenv! 只加载默认.env文件中的配置
 fn use_macro() {
-    println!("macro get value: {}", dotenv!("bb"));
+    // println!("macro get value: {}", dotenv!("dotenv/.env"));
 }
